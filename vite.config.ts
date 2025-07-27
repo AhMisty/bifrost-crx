f0683c7bfe9b111a40cb515323a20d6b79ecfdec
AhMisty <AhMisty@outlook.com>
2025-07-27 20:21:03 +0800

    feat: 🎸 初始化仓库

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { URL, fileURLToPath } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.config'
+import unocss from 'unocss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), vueDevTools(), crx({ manifest }), unocss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'out',
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        assetFileNames: `assets/[ext]/[name].[hash].[ext]`,
+      },
+    },
+  },
+  server: {
+    port: 3721,
+    cors: {
+      origin: [/chrome-extension:\/\//],
+    },
+    hmr: {
+      port: 3721,
+    },
+  },
+})
